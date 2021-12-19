5023745d5257303fe0a39e6bae0de57a9557fa43
DruboSaptapan <saptapan2017@gmail.com>
2021-12-19 23:06:53 +0600

    green-leaf-server

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Moving Service website node mongo server')
+    res.send('Green leaf website server running')
 });
 
 app.listen(port, () => {
